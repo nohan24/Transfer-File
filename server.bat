52f0d068b3b67b112f535e0648e01e7f26fc521f
Nohan <94552970+nohan24@users.noreply.github.com>
2022-12-22 11:08:37 +0300

    Update server.bat

--- server.bat
+++ server.bat
@@ -2,4 +2,8 @@ javac -d . data/*.java
 javac -d . thread/*.java
 javac -d . server/*.java
 javac -d . event/*.java
+javac -d . data/*.java
+javac -d . thread/*.java
+javac -d . server/*.java
+javac -d . event/*.java
 java server.ServerPanel
